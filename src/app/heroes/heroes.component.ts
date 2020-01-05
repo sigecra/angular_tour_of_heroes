17fb0a47b46e0ea4cf2e214900093c7601217b0e
sigecra <sigecra@gmail.com>
2020-01-04 21:47:43 -0400

    Routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
 
 @Component({
@@ -14,16 +13,13 @@ export class HeroesComponent implements OnInit {
   //     name: 'Windstorm' 
   // };
   //heroes = HEROES;
-  selectedHero: Hero;
   heroes: Hero[];
 
   constructor(private heroService: HeroService) { }
   ngOnInit() {
     this.getHeroes();
   }
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
