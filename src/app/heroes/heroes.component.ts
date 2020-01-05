c161ed21b86eb6e9f1edea65e24e516170c31200
sigecra <sigecra@gmail.com>
2020-01-05 19:04:20 -0400

    HTTP actualizacion

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
@@ -8,14 +9,10 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  // hero: Hero = { 
-  //  id: 1, 
-  //     name: 'Windstorm' 
-  // };
-  //heroes = HEROES;
   heroes: Hero[];
 
   constructor(private heroService: HeroService) { }
+
   ngOnInit() {
     this.getHeroes();
   }
@@ -23,6 +20,20 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
   }
 
 }
